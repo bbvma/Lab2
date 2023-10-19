6ac0e46ceb9df491d20cfdbab82f921fb1a53805
bbvma <92751433+bbvma@users.noreply.github.com>
2023-10-19 10:23:16 +0300

    fixed

---
+++ Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2ready
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public List<string> Genres { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public string Annotation { get; set; }
+        public string ISBN { get; set; }
+        public bool KeywordFoundInAnnotation { get; set; }
+
+        //выборка информации о книге, без аннотации
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Название книги: {Title}");
+            Console.WriteLine($"Автор: {Author}");
+            Console.WriteLine($"Жанры: {string.Join(", ", Genres)}");
+            Console.WriteLine($"Дата публикации: {PublicationDate.ToShortDateString()}");
+            Console.WriteLine($"ISBN: {ISBN}");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Catalog.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2ready
+{
+    public class Catalog
+    {
+        private List<Book> books = new List<Book>();
+        public void AddBook(Book book)
+        {
+            books.Add(book);
+        }
+
+        public List<Book> SearchByTitle(string searchByTitle)
+        {
+            return books.Where(book => book.Title.Contains(searchByTitle)).ToList();
+        }
+
+        public List<Book> SearchByAuthor(string searchByAuthor)
+        {
+            return books.Where(book => book.Author.Contains(searchByAuthor)).ToList();
+        }
+
+        public List<Book> SearchByISBN(string searchByISBN)
+        {
+            return books.Where(book => book.ISBN == searchByISBN).ToList();
+        }
+
+        public List<Book> SearchByKeywords(List<string> keywords)
+        {
+            List<Book> result = new List<Book>();
+            foreach (var book in books)
+            {
+                if (keywords.Count(keyword => book.Title.Contains(keyword) || book.Annotation.Contains(keyword)) != 0)
+                    result.Add(book);
+            }
+            return result;
+        }
+    }
+}
